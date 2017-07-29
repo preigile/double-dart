4cb707eba43d79eccc0b6dec12c96fe4941fcd11
Irina Chichikova <diva.artim@gmail.com>
2017-07-29 21:49:40 +0300

    Init numbers for Target

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,29 @@
-// Copyright (c) 2017, treks. All rights reserved. Use of this source code
+// Copyright (c) 2017, preigile. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/platform/browser.dart';
-
 import 'package:double_dart/app_component.dart';
 
 void main() {
-  bootstrap(AppComponent);
+//  bootstrap(AppComponent);
+  print(new Target().nums);
+}
+
+class Target {
+  Map<String, int> nums;
+
+  Target() {
+    nums = new Map();
+    nums['25'] = 25;
+    nums['50'] = 50;
+    initNums();
+  }
+  
+  initNums() {
+    for(var i = 1; i < 21; i++) {
+      nums['${i}'] = i;
+      nums['double${i}'] = i * 2;
+      nums['thrice${i}'] = i * 3;
+    }
+  }
 }
