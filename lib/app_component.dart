6c86ebb801246d377be2342633c3a576ba533328
Irina Chichikova <diva.artim@gmail.com>
2017-07-30 12:18:47 +0300

    Get numbers random

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -4,7 +4,9 @@
 import 'package:angular2/angular2.dart';
 import 'package:angular_components/angular_components.dart';
 
-import 'src/todo_list/todo_list_component.dart';
+import 'dart:math';
+
+//import 'src/todo_list/todo_list_component.dart';
 
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
@@ -13,10 +15,61 @@ import 'src/todo_list/todo_list_component.dart';
   selector: 'my-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [materialDirectives, TodoListComponent],
+  directives: const [materialDirectives],
   providers: const [materialProviders],
 )
 
 class AppComponent {
-  // Nothing here yet. All logic is in TodoListComponent.
+  var play;
+
+  AppComponent() {
+    this.play = new Play();
+//    print(play);
+  }
+
+  void getPoint () {
+    print(play.getPoint());
+  }
+}
+
+class Target {
+  Map<String, int> nums;
+
+  Target() {
+    nums = new Map();
+    nums['25'] = 25;
+    nums['50'] = 50;
+    initNums();
+  }
+
+  initNums() {
+    for(var i = 1; i < 21; i++) {
+      nums['${i}'] = i;
+      nums['double_${i}'] = i * 2;
+      nums['triple_${i}'] = i * 3;
+    }
+  }
+}
+
+class Play {
+  var target;
+  int randomNum;
+  int score;
+  int step;
+
+  Play() {
+    this.target = new Target();
+    step = 0;
+  }
+  
+  getPoint() {
+    if(step < 3) {
+      step++;
+    } else {
+      step = 1;
+    }
+    print('step ${step}');
+    randomNum = target.nums.values.elementAt(new Random().nextInt(target.nums.length));
+    return randomNum;
+  }
 }
--- web/main.dart
+++ web/main.dart
@@ -5,25 +5,6 @@ import 'package:angular2/platform/browser.dart';
 import 'package:double_dart/app_component.dart';
 
 void main() {
-//  bootstrap(AppComponent);
-  print(new Target().nums);
+  bootstrap(AppComponent);
 }
 
-class Target {
-  Map<String, int> nums;
-
-  Target() {
-    nums = new Map();
-    nums['25'] = 25;
-    nums['50'] = 50;
-    initNums();
-  }
-  
-  initNums() {
-    for(var i = 1; i < 21; i++) {
-      nums['${i}'] = i;
-      nums['double${i}'] = i * 2;
-      nums['thrice${i}'] = i * 3;
-    }
-  }
-}
