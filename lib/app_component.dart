3675e22dbdbe65ef8452d16a2bc740755348f024
Irina Chichikova <diva.artim@gmail.com>
2017-07-31 21:43:51 +0300

    Add players

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,10 +1,10 @@
 // Copyright (c) 2017, preigile. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:math';
+
 import 'package:angular2/angular2.dart';
 import 'package:angular_components/angular_components.dart';
-
-import 'dart:math';
 
 //import 'src/todo_list/todo_list_component.dart';
 
@@ -20,16 +20,15 @@ import 'dart:math';
 )
 
 class AppComponent {
-  var play;
+  var game;
 
   AppComponent() {
-    this.play = new Play();
-//    print(play);
+    this.game = new Game();
+    print('Player ${game.playerName} is playing');
   }
 
-  void getPoint () {
-    print(play.getPoint());
-    play.getScore();
+  void getPoint() {
+    game.play();
   }
 }
 
@@ -38,13 +37,14 @@ class Target {
 
   Target() {
     nums = new Map();
+    nums['0'] = 0;
     nums['25'] = 25;
     nums['50'] = 50;
     initNums();
   }
 
   initNums() {
-    for(var i = 1; i < 21; i++) {
+    for (var i = 1; i < 21; i++) {
       nums['${i}'] = i;
       nums['double_${i}'] = i * 2;
       nums['triple_${i}'] = i * 3;
@@ -52,32 +52,63 @@ class Target {
   }
 }
 
-class Play {
+class Player {
+  String name;
+
+  Player(this.name) {}
+}
+
+class Game {
   var target;
+  var player1;
+  var player2;
+
   int randomNum;
   int score;
   int step;
+  String playerName;
 
-  Play() {
+  Game() {
     this.target = new Target();
+    this.player1 = new Player('Vasya');
+    this.player2 = new Player('Jora');
+
     step = 0;
     score = 0;
+    playerName = this.player1.name;
   }
-  
+
   getPoint() {
-    randomNum = target.nums.values.elementAt(new Random().nextInt(target.nums.length));
+    randomNum =
+        target.nums.values.elementAt(new Random().nextInt(target.nums.length));
     return randomNum;
   }
 
   getScore() {
-    if(step < 3) {
+    if (step < 3) {
       step++;
       score += randomNum;
     } else {
+      player();
       step = 1;
       score = randomNum;
     }
-    print('step ${step}');
-    print('Your score is ${score}!');
+
+    print('step ${step}, you get ${randomNum} score');
+    print('Your common score is ${score}!');
+  }
+
+  player() {
+    if (playerName == player1.name) {
+      playerName = player2.name;
+    } else {
+      playerName = player1.name;
+    }
+    print('Player ${playerName} is playing');
+  }
+
+  play() {
+    getPoint();
+    getScore();
   }
 }
